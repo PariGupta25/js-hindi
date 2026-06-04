b7b30f265ef4eee68acb581c65297f25267bad2b
PariGupta25 <parigupta2512006@gmail.com>
2026-06-04 10:11:34 +0000

    configuration done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"// treat all JS code as newer version
+
+//alert(3+3)// we r using nodejs and not browser
+// number=> 2 to the power 53
+// bigint
+// string=""
+//null=> standalone val
+//boolean= true/false
+//undefined= var declare karke chor dia
+//symbols =>unique
+//typeof=>used to find type of data type
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
