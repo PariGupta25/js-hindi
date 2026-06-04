1e624afcbe0dacbedca1cfd6c77834bbe2711279
PariGupta25 <parigupta2512006@gmail.com>
2026-06-04 12:25:15 +0000

    configuration done

---
+++ 01_basics/03_datatypeconversion.js
@@ -0,0 +1,66 @@
+// let score ="33abc"
+// // console.log(typeof score);
+// // console.log(typeof (score));
+
+// let valueinNumber = Number(score)
+// // console.log(typeof valueinNumber)
+// // console.log(valueinNumber);
+
+
+// // Jab NaN aaye that means it is a separate datatype 
+// // "33"= 33
+// // "33abc"= NaN
+// // true=1 false=0
+// //undefined=NaN
+// // Null=0/NaN
+// //"PARI"=NaN
+
+// let isloggedIn="Pari"
+// let BooleanisLoggedIn=Boolean(isloggedIn)
+// // console.log(BooleanisLoggedIn);
+
+
+// // 1=true 0=false
+// //""=false
+// //"pari"=true
+
+// let someNumber=3
+// let stringNumber=string(someNumber)
+// // console.log(typeof stringNumber);
+// // console.log(stringNumber);
+
+//*****************************OPERATIONS**********************************************
+
+let value =3
+let negvalue=-value
+console.log(negvalue);
+
+let str1="Hello"
+let str2=" Pari"
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+3);
+console.log(1+2+"3");
+
+// prefix and postfix
+//int
+let x = 3;
+const y = x++;
+// x is 4; y is 3
+console.log(x);
+console.log(y);
+
+//Big Int
+let x2 = 3n;
+const y2 = x2++;
+// x2 is 4n; y2 is 3n
+console.log(x2);
+console.log(y2);
+
+
+
+
+
