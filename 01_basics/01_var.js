fd19015315afc4f1000aab4409e0556b736d502d
PariGupta25 <parigupta2512006@gmail.com>
2026-06-04 09:45:58 +0000

    config done

---
+++ 01_basics/01_var.js
@@ -0,0 +1,12 @@
+const accountId =181818
+let accountEmail="Pari@gmail.com"
+var accPassword="1919191"
+accountCity="amritsar"
+let accountState 
+//accountId =111222 // ye change nhi kr sakte iss tarah
+accountEmail="Pari123@gmail.com"
+accPassword="1919195671"
+accountCity="paris"  
+console.table([accountEmail,accPassword,accountId,accountCity,accountState])
+/* donot prefer using var because of issue in block or functional scope*/
+// when we leave var after declaring it gets undefined value
