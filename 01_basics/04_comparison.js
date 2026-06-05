693d540d4822ab2837a0faf40f8ae18b64f8b3c4
PariGupta25 <parigupta2512006@gmail.com>
2026-06-05 09:34:19 +0000

    configuration done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>=0)
+// === checks not only values but also their data types
+console.log("2"===2);
