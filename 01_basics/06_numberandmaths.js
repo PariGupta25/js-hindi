d70fe969bca7734ae708477dbcfcd15e0135a9cd
PariGupta25 <parigupta2512006@gmail.com>
2026-06-08 17:06:33 +0000

    configuration done

---
+++ 01_basics/06_numberandmaths.js
@@ -0,0 +1,26 @@
+const score=400
+console.log(score);
+
+const balance =new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const othernumber=123.8966
+console.log(othernumber.toPrecision(4));
+
+const Hundreds=100000
+console.log(Hundreds.toLocaleString('en-In'));
+
+
+//>>>>>>>>>>>>>>>>>>>>MATHS<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min =10
+const max =20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
