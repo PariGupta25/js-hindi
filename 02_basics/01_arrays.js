eeb2a8f3788deb21f1a01465dfe685a64c98935a
PariGupta25 <parigupta2512006@gmail.com>
2026-06-09 09:36:24 +0000

    configuration done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,32 @@
+//arrays
+
+const Arr =[1,2,3,4,5,6]
+// const heroes=["shaktiman","batman","superman"]
+// console.log(Arr[1]);
+
+//ARRAY METHODS
+
+// Arr.push(7)
+// Arr.push(8)
+// Arr.pop()
+
+// Arr.unshift(9)
+// Arr.shift()
+
+// console.log(Arr.includes(9));
+// console.log(Arr.indexOf(4));
+
+// const newArr = Arr.join()  -> CONVERTS arr to string
+
+
+// slice ,splice
+
+// const myn1=Arr.slice(1,3)
+// console.log("B",Arr);
+// console.log(myn1);
+// const myn2=Arr.splice(1,3)
+// console.log("C",Arr);
+// console.log(myn2);
+// console.log(newArr);
+
+
---
+++ 02_basics/02_arraypart2.js
@@ -0,0 +1,22 @@
+const marvelheros=["thor","ironman","spiderman"]
+const dc_heros=["superman","flash","batman"]
+
+// marvelheros.push(dc_heros)
+
+// // console.log(marvelheros);
+// // console.log(marvelheros[3][1]);
+
+// const allheros = marvelheros.concat(dc_heros)
+// console.log(allheros);
+
+// const all_new = [...marvelheros,...dc_heros]
+// console.log(all_new);
+
+// console.log(Array.isArray("Pari"))
+
+// console.log(Array.from("Pari"));
+
+let score1 =100
+let score2 =200
+let score3 =300
+console.log(Array.of (score1,score2,score3));
