a5a336ab6960ac6a27ee4b1ff71d3568e6812b89
PariGupta25 <parigupta2512006@gmail.com>
2026-06-20 17:09:40 +0000

    code configuration done

---
+++ 05_iterations/01_one.js
@@ -0,0 +1,25 @@
+//for
+let element;
+// for (let index = 0; index <10; index++) {
+//     const element = index;
+//     console.log(element);
+// }
+// console.log(element);
+
+
+// for (let index = 0; index <=10; index++) {
+//     console.log(`Outer loop ${index}`);
+//     for (let j = 0; j<=10 ; j++) {
+//         console.log(`inner loop ${j}`);
+//     }
+// }
+
+//ARRAY
+let myArray=["batman","superman","spiderman"];
+// console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+    
+}
